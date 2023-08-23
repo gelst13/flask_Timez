f319d2619d63b2574243ed58776585adb5f03c2c
Gelst13 <Gelst13@rambler.ru>
2023-08-23 15:45:46 +0300

    Created - pc.py for tasks and pseudocode

---
+++ pc.py
@@ -0,0 +1,15 @@
+"""
+- error in add new contact
+
+- export_contacts function
+? email file or save file locally(both)
+
+Time operations block
+0: find error
+In 5 hours 0 minutes it'll be: 1 day
+-add calculate time func
+
+- write unittest
+
+
+"""
